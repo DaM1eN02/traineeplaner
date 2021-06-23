2b5b318f86bdf4ee36d12dc45f8f8b3ce588d2af
Damien Arriens <77632987+DaM1eN02@users.noreply.github.com>
2021-06-23 15:54:12 +0200

    Major Updates

--- pages/planer.js
+++ pages/planer.js
@@ -1,5 +1,17 @@
 function options() {
-    let node = document.getElementById('list');
-    let visibility = node.style.visibility;
-    node.style.visibility = visibility === "visible" ? 'hidden' : 'visible';
+    let modal = document.getElementById("myModal");
+
+    let span = document.getElementsByClassName("close")[0];
+
+    modal.style.display = "block";
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
 }
